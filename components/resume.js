9cece5c8a22420343fc227d34d18a39483b0cfa1
chris-melvin <chrismelvin.04@gmail.com>
2022-02-04 18:12:11 +0800

    updated resume

--- components/resume.js
+++ components/resume.js
@@ -2,7 +2,7 @@ import { AiOutlineFilePdf } from "react-icons/ai";
 export default function () {
   return (
     <div className=" bg-primary  fixed z-50 right-0 top-5 ">
-      <a className="text-3xl" href="/Resume.pdf">
+      <a className="text-3xl" href="/Resume_latest.pdf">
         <AiOutlineFilePdf />
       </a>
     </div>
